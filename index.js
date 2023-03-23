e37a289df1bf92a33d1ab7d00a737937b74a5b48
Nishijama <nathanplociennik@gmail.com>
2023-03-23 01:11:15 +0100

    Update after almost a year-long break.

--- index.js
+++ index.js
@@ -1,53 +1,143 @@
-// TODO: Update local storage after re-arranging
 
+let TABLECAPACTIY = 4;
+let GUESTCOUNT = 0;
+let NUMBEROFTABLES = 10
 
+createTables(NUMBEROFTABLES);
 const tables = document.querySelectorAll(".table");
-let tableCapacity = 4;
-let GUEST_COUNT = 0;
 
-if (!localStorage.getItem("guest_list")) {
-    loadGuests();
-}
-// fetchGuests();  Relic
-populateTables();
+// if (!localStorage.getItem("guest_list")) {
+//     loadGuests();
+// }
+// loadGuests();
 
+let PLAN = [];
+PLAN = populateTables();
 
 window.onload = () => {
 checkTableCapacity();
 }
 
-
 // FUNCTION DEFINITIONS
 
-async function loadGuests() {
-    const response = await fetch("base.json");
-    const data = await response.json();
-    localStorage.setItem("guest_list", JSON.stringify(data));
+function createTables(n) {
+    let tablesContainer = document.querySelector(".tablesContainer")
+    for (let i = 0; i < n; i++) {
+        let tableWrapper = document.createElement('div');
+        tableWrapper.classList.add('table-wrapper');
+
+        let table = document.createElement('div')
+        table.classList.add('table');
+        table.setAttribute('id', 'table' + (i+1));
+        let heading = document.createElement('p');
+        heading.classList.add('tableHeading')
+        table.appendChild(heading);
+        heading.innerHTML =`<strong>Table ${i+1}</strong`;
+
+        let countBox = document.createElement('p')
+        countBox.innerHTML = '0'
+
+        tableWrapper.appendChild(table);
+        tableWrapper.appendChild(countBox);
+        tablesContainer.appendChild(tableWrapper)
+    }
 }
 
+
+// Method to upload a valid excel file
+function upload() {
+    var files = document.getElementById('file_upload').files;
+    if(files.length==0){
+      alert("Please choose any file...");
+      return;
+    }
+    var filename = files[0].name;
+    var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+    if (extension == '.XLS' || extension == '.XLSX') {
+        excelFileToJSON(files[0]);
+    }else{
+        alert("Please select a valid excel file.");
+    }
+  }
+   
+  //Method to read excel file and convert it into JSON 
+function excelFileToJSON(file){
+  try {
+    var reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = function(e) {
+
+        var data = e.target.result;
+        var workbook = XLSX.read(data, {
+            type : 'binary'
+        });
+        var result = {};
+        workbook.SheetNames.forEach(function(sheetName) {
+            var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName], {defval: ""});
+            if (roa.length > 0) {
+                result[sheetName] = roa;
+            }
+        });
+        //displaying the json result
+        console.log(result);
+        // console.log(JSON.stringify(result.Sheet1, null, 4)); 
+        localStorage.setItem("guest_list", JSON.stringify(result.Sheet1, null, 4));
+        location.reload();
+        }
+    }catch(e){
+        console.error(e);
+    }
+}
+
+
+// Load json file into the local storage 
+// async function loadGuests() {
+//     const response = await fetch("database.json");
+//     const data = await response.json();
+//     localStorage.setItem("guest_list", JSON.stringify(data));
+// }
+
 function populateTables() {
-    var retrievedGuestList = localStorage.getItem('guest_list');
+    let retrievedGuestList = localStorage.getItem('guest_list');
     let guestList = JSON.parse(retrievedGuestList);
     console.log('Guest List: ', guestList);
 
     guestList.forEach(object => {
         let guestTag = document.createElement('p');
         guestTag.setAttribute('draggable', 'true');
-        guestTag.setAttribute('id', object.id);
+        guestTag.setAttribute('id', object.PersonID);
         guestTag.classList.add('draggable', 'guestTag');
-        guestTag.dataset.id = object.id
-        guestTag.dataset.name = object.name;
-        guestTag.dataset.house = object.house;
-        // guestTag.dataset.with = object.with;  TODO
-        guestTag.dataset.table = object.table;
-        // guestTag.dataset.arrived = object.arrived;  TODO
-        guestTag.innerHTML = object.name;
-        document.getElementById("table" + object.table).appendChild(guestTag)
+        guestTag.dataset.PersonID = object.PersonID
+        guestTag.dataset.Name = object.Name;
+        guestTag.dataset.CompanyName = object.CompanyName;
+        guestTag.dataset.Role = object.Role;
+        guestTag.dataset.Sector = object.Sector;
+        guestTag.dataset.Guest = object.Guest;
+        guestTag.dataset.PRH = object.PRH;
+        guestTag.dataset.TableNumber = object.TableNumber;
+        guestTag.dataset.Seats = object.Seats;
+
+
+        if (guestTag.dataset.CompanyName == "McKinsey") {
+            guestTag.style.backgroundColor = "Blue";
+            guestTag.style.color = "White";
+        }
+
+        if(guestTag.dataset.Guest != "") {
+            guestTag.innerHTML = object.Name + "<br /> " + object.Guest
+        } else {
+            guestTag.innerHTML = object.Name;
+        }
+
+        document.getElementById("table" + object.TableNumber).appendChild(guestTag)
         handleDragging();
-        GUEST_COUNT+=1;
+        GUESTCOUNT+=1;
+
     });
-    console.log(GUEST_COUNT);
+    console.log(GUESTCOUNT);
+    return guestList;
 }
+
 
 function getDragAfterElement(table, y) {
     const draggableElements = [...table.querySelectorAll('.draggable:not(.dragging)')]
@@ -64,92 +154,49 @@ function getDragAfterElement(table, y) {
 }
 
 
-
 function checkTableCapacity() {
     tables.forEach(table => {
-        let numb = table.childElementCount-1;
-        if (numb > tableCapacity) {
+        let count = 0;
+        for (const child of table.children) {
+            if(child.classList.contains("guestTag"))
+            count += parseInt(child.dataset.Seats)
+        }
+        console.log(count);
+        table.nextSibling.innerHTML = count;
+
+        if (count > TABLECAPACTIY) {
             table.style.borderColor ="red";
         }
-        if (numb <= tableCapacity){
+        if (count <= TABLECAPACTIY){
             table.style.borderColor ="black";
         }
-    });
-}
+        })
+    }
 
-// async function fetchGuests() {
-//     const response = await fetch("base.json");
-//     const data = await response.json();
-//     data.forEach(object => {
-//         // console.log(object.name);
-//         let guestTag = document.createElement('p');
-//         guestTag.setAttribute('draggable', 'true');
-//         guestTag.setAttribute('id', object.id);
-//         guestTag.classList.add('draggable', 'guestTag');
-//         guestTag.innerHTML = object.name;
-//         guestTag.dataset.name = object.name;
-//         guestTag.dataset.table = object.table;
-//         guestTag.dataset.house = object.house;
-//         document.getElementById("table" + object.table).appendChild(guestTag)
-//         handleDragging();
-//         GUEST_COUNT+=1;
-//     });
-//     console.log(GUEST_COUNT);
-// }
-
-function showHouses () {
+function showCompanies () {
     guests = document.querySelectorAll('.guestTag');
     guests.forEach(guest => {
-        guest.innerHTML = guest.dataset.house;
+        guest.innerHTML = guest.dataset.CompanyName;
     });
 }
+
+function showSectors () {
+    guests = document.querySelectorAll('.guestTag');
+    guests.forEach(guest => {
+        guest.innerHTML = guest.dataset.Sector;
+    });
+}
+
 
 function showNames () {
     guests = document.querySelectorAll('.guestTag');
     guests.forEach(guest => {
-        guest.innerHTML = guest.dataset.name;
-    });
-}
-
-
-document.getElementById("AddGuestBtn").addEventListener("click", function(event) {
-    createNameTag();
-    event.preventDefault();
-})
-
-function createNameTag() {
-    let guestTag = document.createElement('p');
-    guestTag.dataset.id = GUEST_COUNT+1;
-    guestTag.dataset.name = document.getElementById("AddGuestName").value;
-    guestTag.dataset.table = document.getElementById("AddGuestTable").value;
-    guestTag.dataset.house = document.getElementById("AddGuestHouse").value;
-    guestTag.dataset.with = ""; // TODO
-    guestTag.dataset.arrived = ""; // TODO
-
-    let newGuest = {
-        id: guestTag.dataset.id,
-        name: guestTag.dataset.name,
-        table: guestTag.dataset.table,
-        house: guestTag.dataset.house,
-        with: guestTag.dataset.with,
-        arrived: guestTag.dataset.arrived
+    if(guest.dataset.Guest != "") {
+        guest.innerHTML = guest.dataset.Name + "<br /> " + guest.dataset.Guest
+    } else {
+        guest.innerHTML = guest.dataset.Name;
     }
-    let retrievedGuestList = localStorage.getItem('guest_list');
-    let guestList = JSON.parse(retrievedGuestList);
-    guestList.push(newGuest)
-    localStorage.setItem("guest_list", JSON.stringify(guestList));
-
-    guestTag.innerHTML = guestTag.dataset.name;
-    guestTag.setAttribute('id', guestTag.dataset.id);
-    guestTag.setAttribute('draggable', 'true');
-    guestTag.classList.add('draggable', 'guestTag');
-    document.getElementById("table" + guestTag.dataset.table).appendChild(guestTag)
-    document.getElementById("AddGuestTable").value = "";
-    document.getElementById("AddGuestHouse").value = "";
-    document.getElementById("AddGuestName").value = "";
-    handleDragging();
-    GUEST_COUNT+=1;
-    console.log(GUEST_COUNT);
+    });
 }
 
 function handleDragging() {
@@ -165,7 +212,7 @@ function handleDragging() {
         draggable.addEventListener("dragend", () =>{
             draggable.classList.remove("dragging")
             checkTableCapacity();
-
+            PLAN = updatePLAN();
         })
     })
 
@@ -178,20 +225,63 @@ function handleDragging() {
             const currentDraggable = document.querySelector('.dragging');
             if (afterElement == null) {
                 table.appendChild(currentDraggable);
-                currentDraggable.dataset.table = table.id;
+                currentDraggable.dataset.TableNumber = (table.id).slice(-1);
             } else {
                 table.insertBefore(currentDraggable, afterElement)
-                currentDraggable.dataset.table = table.id;
+                currentDraggable.dataset.TableNumber = (table.id).slice(-1);
             }
         });
         
     });
 }
 
-function saveToJSON() {
-    
+function updatePLAN() {
+    const guestTags = document.querySelectorAll(".guestTag");
+    let newPLAN = []
+    guestTags.forEach(tag => {
+
+        let object = new Object()
+        object.PersonID = tag.dataset.PersonID;
+        object.Name = tag.dataset.Name;
+        object.CompanyName = tag.dataset.CompanyName;
+        object.Role = tag.dataset.Role;
+        object.Sector = tag.dataset.Sector;
+        object.Guest = tag.dataset.Guest;
+        object.PRH = tag.dataset.PRH;
+        object.TableNumber = tag.dataset.TableNumber;
+        object.Seats = tag.dataset.Seats;
+
+        newPLAN.push(object)
+    })
+    return newPLAN;
 }
 
-function saveToLocalStorage() {
+let saveBtn = document.getElementById("saveBtn");
+saveBtn.addEventListener("click", () => {
+    localStorage.setItem("guest_list", JSON.stringify(PLAN));
+    downloadObjectAsXlsx()
+    alert("Saved!")
+})
 
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
+function downloadObjectAsXlsx() {
+    let retrievedGuestList = localStorage.getItem('guest_list');
+    let guestList = JSON.parse(retrievedGuestList);
+    console.log(guestList);
+    console.log(typeof(guestList));
+    const worksheet = XLSX.utils.json_to_sheet(guestList);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+    /* create an XLSX file and try to save to Presidents.xlsx */
+    XLSX.writeFile(workbook, "guest_list.xlsx", { compression: true });
 }
--- readme.md
+++ readme.md
@@ -1,21 +1,14 @@
-# Event Planner App
+# Seating planner app
 
-### This project is still in progress
-Current page is just one part of a bigger event planner app I'm planning to build
+### It's a very pragmatic app that will help my team plan table arrangements at big events
 
 Current features:
-* read initial data from JSON file
-* upload data to local storage
-* populate tables with guests
-* re-arrange sitting plan by drag-and-dropping guests
-* switch view to show other info than names (e.g., houses)
-* add new guests to the tables
-* notify when sitting arrangement exceeds table's capacity
 
-![screenshot](static/screenshot_2022-08-03.png)
+- upload data from an xlsx file
+- upload data to local storage
+- populate tables with guests
+- re-arrange sitting plan by drag-and-dropping guests
+- switch view to show other info than names (e.g., Company, Sector)
+- notify when sitting arrangement exceeds table's capacity
 
-Future features:
-* use Vue/React
-* build back-end in Django
-* use sqlite3 database
-* implement other parts of the app (tbc.)
+![screenshot](static/Screenshot 2023-03-23 at 01.09.26.png)
